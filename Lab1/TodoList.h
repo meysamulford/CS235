ddbe5000daddc2939c9afc53581df72fd7aab70f
meysamulford <meysamulford@gmail.com>
2021-01-23 05:51:39 +0000

    created extensions to take from a file and output to a file. Also define the add, remove, and print functions.

--- Lab1/TodoList.h
+++ Lab1/TodoList.h
@@ -4,23 +4,49 @@
 #include <iostream>
 #include <string>
 #include "TodoListInterface.h"
+#include <fstream>
+#include <vector>
+//#include <ofstream>
 
 using namespace std;
 
 class TodoList : public TodoListInterface {
 public:
+		vector <string> daysList;
+		vector <string> taskList;
+		//constructor
     TodoList() {
 			cout << "This is constructor" << endl;
-		} //constructor
+			ifstream infile ("TODOList.txt");
+			string line;
+			if (infile.is_open()) {
+				while (getline (infile, line)) {
+					cout << line << endl;
+					daysList.push_back(line);
+					taskList.push_back(line);
+				}
+			}
+			infile.close();		
+		} 
+		//destructor
     virtual ~TodoList() {
 			cout << "This is destructor" << endl;
-		} //destructor
+			ofstream outfile;
+			outfile.open("TODOList.txt", ofstream::out | ofstream::trunc);
+			for (int i = 0; i < daysList.size(); ++i) {
+				outfile << daysList[i] << endl;
+				outfile << taskList[i] << endl;
+			}
+			outfile.close();
+		}
 
     /*
     *   Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"
     */
     virtual void add(string _duedate, string _task) {
 			cout << "This is add" << endl;
+			daysList.push_back(_duedate);
+			taskList.push_back(_task);
 		}
 
     /*
@@ -30,13 +56,28 @@ public:
     */
     virtual int remove(string _task) {
 			cout << "This is remove" << endl;
+			unsigned int i = 0;
+			bool found = false;
+			while(!found && (i < taskList.size())) {
+				if(taskList.at(i) == _task) {
+					daysList.erase(daysList.begin() + i);
+					taskList.erase(taskList.begin() + i);
+					found = true;
+				}
+				++i;
+			}
 		}
+		//substr -> start after the space
 
     /*
     *   Prints out the full todo list to the console
     */
     virtual void printTodoList() {
 			cout << "This is print" << endl;
+			for(int i = 0; i < daysList.size(); ++i) {
+				cout << daysList[i] << endl;
+				//cout << taskList[i] << endl;
+			}
 		}
     
     /*
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -14,5 +14,19 @@ int main(int argc, char *argv[]) {
 		string date = argv[2];
 		string task = argv[3];
 		cout << "date: " << date << " task: " << task << endl;
+		myList.add(date, task);
+	}
+	if(firstarg.compare("remove")==0) {
+		cout << "Doing a remove" << endl;
+		string task = argv[2];
+		myList.remove(task);
+	}
+	if(firstarg.compare("print")==0) {
+		cout << "Doing a print" << endl;
+		myList.printTodoList();
+	}
+	if(firstarg.compare("printDaysTasks")==0) {
+		cout << "Doing a print for single day" << endl;
+		//not sure how this one is going to work
 	}
 }
